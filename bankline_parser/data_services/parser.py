3007086bf88d1537b06c716932ee283261e0c11d
Igor <igor.ushkarev@digital.justice.gov.uk>
2018-03-05 14:04:09 +0000

    Improve code style

--- bankline_parser/data_services/parser.py
+++ bankline_parser/data_services/parser.py
@@ -3,42 +3,43 @@ from .exceptions import ParseError
 from .utils import IndexTrackingIterator
 
 
+def _parse_account(lines_iter):
+    # check for end of iteration on opening label of next account
+    try:
+        file_header_label = models.FileHeaderLabel(next(lines_iter))
+    except StopIteration:
+        return None
+
+    user_header_label = models.UserHeaderLabel(next(lines_iter))
+
+    # parse records until user_trailer_label
+    records = []
+    while True:
+        current_row = next(lines_iter)
+        try:
+            # try and parse final row
+            user_trailer_label = models.UserTrailerLabel(current_row)
+            return models.Account(file_header_label, user_header_label,
+                                  records, user_trailer_label)
+        except ParseError:
+            # if failed to parse final row, continue to parse records
+            record = models.BaseRecord(current_row)
+            if record.transaction_code is models.TransactionCode.balance_record:
+                record = models.BalanceRecord(current_row)
+            else:
+                record = models.DataRecord(current_row)
+            records.append(record)
+
+
 def parse(iterable):
     lines_iter = IndexTrackingIterator(iterable)
-
-    def parse_account(lines_iter):
-        # check for end of iteration on opening label of next account
-        try:
-            file_header_label = models.FileHeaderLabel(next(lines_iter))
-        except StopIteration:
-            return None
-
-        user_header_label = models.UserHeaderLabel(next(lines_iter))
-
-        # parse records until user_trailer_label
-        records = []
-        while True:
-            current_row = next(lines_iter)
-            try:
-                # try and parse final row
-                user_trailer_label = models.UserTrailerLabel(current_row)
-                return models.Account(file_header_label, user_header_label,
-                                      records, user_trailer_label)
-            except ParseError:
-                # if failed to parse final row, continue to parse records
-                record = models.BaseRecord(current_row)
-                if record.transaction_code is models.TransactionCode.balance_record:
-                    record = models.BalanceRecord(current_row)
-                else:
-                    record = models.DataRecord(current_row)
-                records.append(record)
 
     try:
         volume_header_label = models.VolumeHeaderLabel(next(lines_iter))
 
         accounts = []
         while True:
-            account = parse_account(lines_iter)
+            account = _parse_account(lines_iter)
             if account:
                 accounts.append(account)
             else:
